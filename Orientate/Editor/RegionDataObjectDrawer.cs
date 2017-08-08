99bd252a93a12a69fda68e038379fbf266a65ffd
zouhangte <zouhangtezbm@126.com>
2017-08-08 16:49:56 +0800

    fixed region drawer

---
+++ Orientate/Editor/RegionDataObjectDrawer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEditor;
+using UnityEngine;
+namespace RegionSwitch
+{
+    [CustomEditor(typeof(RegionDataObject))]
+    public class RegionDataObjectDrawer : Editor
+    {
+        RegionDataObject _instence;
+
+        private void OnEnable()
+        {
+            _instence = target as RegionDataObject;
+        }
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            base.OnInspectorGUI();
+            using (var hor = new EditorGUILayout.HorizontalScope())
+            {
+                if (GUILayout.Button("加载信息"))
+                {
+                    LoadData();
+                }
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        public void LoadData()
+        {
+            GameObject root = new GameObject(_instence.name);
+            var record = root.AddComponent<RegionTransformRecord>();
+            foreach (var floor in _instence.floorDataObjs)
+            {
+                GameObject floorItem = new GameObject(floor.name);
+                IconUtility.SetIcon(floorItem, IconUtility.Icon.DiamondYellow);
+                floorItem.transform.SetParent(root.transform);
+                floorItem.transform.position = new Vector3(0, floor.flourHeight, 0);
+
+                foreach (var region in floor.regionList)
+                {
+                    GameObject regionRoot = new GameObject(region.name);
+                    IconUtility.SetIcon(regionRoot, IconUtility.LabelIcon.Orange);
+                    regionRoot.transform.SetParent(floorItem.transform);
+                    regionRoot.transform.localPosition = new Vector3(region.rect.x, 0, region.rect.z);
+
+                    GameObject regionTarget = new GameObject("[" + region.name + "]");
+                    IconUtility.SetIcon(regionTarget, IconUtility.LabelIcon.Red);
+                    regionTarget.transform.SetParent(regionRoot.transform);
+                    regionTarget.transform.localPosition = new Vector3(region.rect.length, 1, region.rect.weight);
+                }
+            }
+            record.regionObj = _instence;
+        }
+    }
+}
--- Orientate/Editor/RegionTransformDrawer.cs
+++ Orientate/Editor/RegionTransformDrawer.cs
@@ -24,16 +24,8 @@ namespace RegionSwitch
         }
         public override void OnInspectorGUI()
         {
-            serializedObject.Update();
             base.OnInspectorGUI();
-            using (var hor = new EditorGUILayout.HorizontalScope())
-            {
-                if (GUILayout.Button("加载信息"))
-                {
-                    LoadData();
-                }
-            }
-            if (_instence.regionObj != null)
+            if (_instence != null)
             {
                 using (var grid = new EditorGUILayout.VerticalScope())
                 {
@@ -57,36 +49,7 @@ namespace RegionSwitch
                     }
                 }
             }
-            serializedObject.ApplyModifiedProperties();
-        }
-
-        public void LoadData()
-        {
-            if (_instence.regionObj == null)
-            {
-                return;
-            }
-            GameObject root = new GameObject(_instence.regionObj.name);
-            foreach (var floor in _instence.regionObj.floorDataObjs)
-            {
-                GameObject floorItem = new GameObject(floor.name);
-                IconUtility.SetIcon(floorItem, IconUtility.Icon.DiamondYellow);
-                floorItem.transform.SetParent(root.transform);
-                floorItem.transform.position = new Vector3(0, floor.flourHeight, 0);
-
-                foreach (var region in floor.regionList)
-                {
-                    GameObject regionRoot = new GameObject(region.name);
-                    IconUtility.SetIcon(regionRoot, IconUtility.LabelIcon.Orange);
-                    regionRoot.transform.SetParent(floorItem.transform);
-                    regionRoot.transform.localPosition = new Vector3(region.rect.x, 0, region.rect.z);
-
-                    GameObject regionTarget = new GameObject("[" + region.name + "]");
-                    IconUtility.SetIcon(regionTarget, IconUtility.LabelIcon.Red);
-                    regionTarget.transform.SetParent(regionRoot.transform);
-                    regionTarget.transform.localPosition = new Vector3(region.rect.length, 1, region.rect.weight);
-                }
-            }
         }
     }
+   
 }
